9d077b779a44f885d19b6427ea89b5876d572e1f
Beatriz Ferreira <biatizferreira04@gmail.com>
2025-03-10 23:48:50 +0000

    basic algo in python and general structure

---
+++ assign1/src/matrixproduct.py
@@ -0,0 +1,39 @@
+import pypapi as papi
+import numpy as np
+import time
+
+def on_mult(m_ar, m_br):
+    # Array initialization
+    A = np.ones((m_ar, m_ar))  
+    B = np.array([[i + 1 for j in range(m_br)] for i in range(m_br)], dtype=float)  
+    C = np.zeros((m_ar, m_br))  
+
+    # matrix multiplication
+    for i in range(m_ar):
+        for j in range(m_br):
+            temp = 0
+            for k in range(m_ar):
+                temp += A[i, k] * B[k, j]
+            C[i, j] = temp
+
+def on_mult_line(m_ar, m_br):
+   ## do multline
+   return
+
+if __name__ == "__main__":
+    while True:
+        print("\n1. Multiplication")
+        print("2. Line Multiplication")
+        print("0. Exit")
+        op = int(input("Selection?: "))
+
+        if op == 0:
+            break
+        
+        lin = int(input("Dimensions (lins=cols)?: "))
+        col = lin
+
+        if op == 1:
+            on_mult(lin, col)
+        elif op == 2:
+            on_mult_line(lin, col)

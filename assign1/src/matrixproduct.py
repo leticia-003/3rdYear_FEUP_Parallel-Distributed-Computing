87627ecbef85a5aee421eebef5692fbcbd23c784
Beatriz Ferreira <biatizferreira04@gmail.com>
2025-03-11 00:04:33 +0000

    multline algo and result printing

--- assign1/src/matrixproduct.py
+++ assign1/src/matrixproduct.py
@@ -5,7 +5,7 @@ import time
 def on_mult(m_ar, m_br):
     # Array initialization
     A = np.ones((m_ar, m_ar))  
-    B = np.array([[i + 1 for j in range(m_br)] for i in range(m_br)], dtype=float)  
+    B = np.ones([[i + j for j in range(m_br)] for i in range(m_br)], dtype=float)  
     C = np.zeros((m_ar, m_br))  
 
     # matrix multiplication
@@ -15,10 +15,24 @@ def on_mult(m_ar, m_br):
             for k in range(m_ar):
                 temp += A[i, k] * B[k, j]
             C[i, j] = temp
+    
+    print("Result matrix:", C[0, :min(10, m_br)])
 
 def on_mult_line(m_ar, m_br):
-   ## do multline
-   return
+   # Array initialization
+    A = np.ones((m_ar, m_ar))  
+    B = np.array([[i + j for j in range(m_br)] for i in range(m_br)], dtype=float)  
+    C = np.zeros((m_ar, m_br))
+
+    #Row wise multiplication
+    for i in range(m_ar):
+        for k in range(m_ar):
+            temp = A[i,k]
+            for j in range(m_br):
+                C[i,j] += temp * B[k,j]
+    
+    print("Result matrix:", C[0, :min(10, m_br)])
+   
 
 if __name__ == "__main__":
     while True:

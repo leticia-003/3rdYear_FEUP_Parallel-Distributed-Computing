065eee668a8fef02b650e9eb40abc3055475ebee
Ana Beatriz Carneiro Ferreira <up202205612@edu.fe.up.pt>
2025-03-18 11:52:10 +0000

    fixed python approach to correctly use normal matrices and matched B to cpp approach for uniformity

--- assign1/src/matrixproduct.py
+++ assign1/src/matrixproduct.py
@@ -3,7 +3,7 @@ import time
 def on_mult(m_ar, m_br):
     # Array initialization
     A = [[1.0 for _ in range(m_ar)] for _ in range(m_ar)]
-    B = [[float(i + j) for j in range(m_br)] for i in range(m_br)]
+    B = [[float(i + 1) for _ in range(m_br)] for i in range(m_br)]
     C = [[0.0 for _ in range(m_br)] for _ in range(m_ar)]
     
     start_time = time.perf_counter()
@@ -13,22 +13,22 @@ def on_mult(m_ar, m_br):
         for j in range(m_br):
             temp = 0
             for k in range(m_ar):
-                temp += A[i, k] * B[k, j]
-            C[i, j] = temp
+                temp += A[i][k] * B[k][j]
+            C[i][j] = temp
     
     #stop counters
     end_time = time.perf_counter()
     
     #results
     print(f"Time: {end_time - start_time:.6f} seconds")
-    print("Result matrix:", C[0, :min(10, m_br)])
+    print("Result matrix:", C[0][:min(10, m_br)])
 
 
 
 def on_mult_line(m_ar, m_br):
     # Array initialization
     A = [[1.0 for _ in range(m_ar)] for _ in range(m_ar)]
-    B = [[float(i + j) for j in range(m_br)] for i in range(m_br)]
+    B = [[float(i + 1) for _ in range(m_br)] for i in range(m_br)]
     C = [[0.0 for _ in range(m_br)] for _ in range(m_ar)] 
 
     
@@ -38,16 +38,16 @@ def on_mult_line(m_ar, m_br):
 
     for i in range(m_ar) :
         for k in range(m_ar) :
-            temp = A[i, k]
+            temp = A[i][k]
             for j in range(m_br) :
-                C[i,j] += temp * B[k, j]
+                C[i][j] += temp * B[k][j]
 
     #stop counters
     end_time = time.perf_counter()
     
     #results
     print(f"Time: {end_time - start_time:.6f} seconds")
-    print("Result matrix:", C[0, :min(10, m_br)])
+    print("Result matrix:", C[0][:min(10, m_br)])
     
    
 
